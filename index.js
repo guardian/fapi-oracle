b1f0bab5bdb1c2cec149871d2a1cfc6e2e193e94
Fabio Crisci <fabio.crisci@guardian.co.uk>
2015-05-15 12:00:57 +0100

    Compare two paths or a list

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 #!/usr/bin/env node
-var argv = require('minimist')(process.argv.slice(2));
+var argv = require('minimist')(process.argv.slice(2), {
+	boolean: ['c']
+});
 var colour = require('colour');
 
 if (argv._.length !== 2 || !argv.o){
-	console.log('Usage:\n   fapi-oracle host1 host2 [-p priority] -o output.diff');
+	console.log([
+		'Usage:',
+		'   fapi-oracle host1 host2 [-p priority] [-s source] -o output.diff',
+		'to compare a list of paths against two hosts',
+		'or',
+		'   fapi-oracle -c host1/path1 host2/path2 -o output.diff',
+		'to compare two full paths'
+	].join('\n'));
 	return process.exit(1);
 }
 
-require('./lib/run')(argv._[0], argv._[1], argv.p, argv.o, argv.l, argv.parallel)
+require('./lib/run')({
+	host1: argv._[0],
+	host2: argv._[1],
+	priority: argv.p,
+	output: argv.o,
+	source: argv.s,
+	limit: argv.l,
+	parallel: argv.parallel,
+	compare: !!argv.c
+})
 .then(function () {
 	console.log('[DONE]'.green + ' Check the output of ' + argv.o.grey);
 })
